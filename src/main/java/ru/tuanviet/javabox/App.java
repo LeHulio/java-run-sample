2dd0c0d4e8918e34a7e0314da131c019edcdd4af
Alex Lasitsa <alexmers911@mail.ru>
2021-11-24 20:51:03 +0300

    add benchmark realisation

--- src/main/java/ru/tuanviet/javabox/App.java
+++ src/main/java/ru/tuanviet/javabox/App.java
@@ -1,26 +1,13 @@
 package ru.tuanviet.javabox;
 
-import ru.tuanviet.javabox.parsers.TopNewsArrayParser;
-
-import java.util.List;
-
 public class App {
-    private static final String ADRESS = "https://hacker-news.firebaseio.com/v0/topstories.json";
     public static void main(String[] args) {
-        App.start();
+
+
 
     }
 
-    public static void start() {
-        String response = new HttpClient(ADRESS).getResponse();
-        TopNews topNews = new TopNews(new TopNewsArrayParser(response).getParsedNews());
-        List<DetailedNews> detailedNews = new HttpFieldsFilter().getFilteredList(topNews);
-        ListFormatter listFormatter = new ListFormatter();
-        List<DetailedNews> filteredDetailedNews = listFormatter.fieldsFormatter(detailedNews);
-        for (DetailedNews news : filteredDetailedNews) {
-            System.out.println(news + "\n");
-        }
-
-
+    public static long multiply(long a, long b) {
+        return a*b;
     }
 }
--- src/test/java/ru/tuanviet/javabox/AppTest.java
+++ src/test/java/ru/tuanviet/javabox/AppTest.java
@@ -1,11 +1,14 @@
 package ru.tuanviet.javabox;
 
 
-import org.junit.Before;
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.Test;
 
 public class AppTest {
 
-
-
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertThat(true).isTrue();
+    }
 }
